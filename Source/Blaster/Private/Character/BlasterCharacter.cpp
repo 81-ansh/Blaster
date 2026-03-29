f52bbd5003c61eaefd8862ff6711eab169b2a70e
Ansh Sharma <anshjangra15@gmail.com>
2026-03-29 22:09:26 +0530

    Character Movement

--- Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -2,13 +2,15 @@
 
 
 #include "Character/BlasterCharacter.h"
-
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ABlasterCharacter::ABlasterCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(GetMesh());
@@ -19,23 +21,51 @@ ABlasterCharacter::ABlasterCharacter()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
 	
-}
-
-void ABlasterCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-void ABlasterCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Move);
+		Input->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Look);
+		Input->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+	}
 
 }
 
+void ABlasterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (InputMapping)
+			{
+				Subsystem->AddMappingContext(InputMapping, 0);
+			}
+		}
+	}
+}
+
+void ABlasterCharacter::Move(const FInputActionInstance& Instance)
+{
+	FVector2D MovementDirection = Instance.GetValue().Get<FVector2D>();
+	const FRotator Rotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+	const FVector ForwardDirection(FRotationMatrix(Rotation).GetUnitAxis(EAxis::X));
+	const FVector RightDirection(FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y));
+	AddMovementInput(RightDirection, MovementDirection.X);
+	AddMovementInput(ForwardDirection, MovementDirection.Y);
+	
+}
+
+void ABlasterCharacter::Look(const FInputActionInstance& Instance)
+{
+	FVector2D LookDirection = Instance.GetValue().Get<FVector2D>();
+	AddControllerYawInput(LookDirection.X);
+	AddControllerPitchInput(LookDirection.Y);
+}
--- Source/Blaster/Public/Character/BlasterCharacter.h
+++ Source/Blaster/Public/Character/BlasterCharacter.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 #include "GameFramework/Character.h"
 #include "BlasterCharacter.generated.h"
 
@@ -15,13 +17,26 @@ public:
 	
 	ABlasterCharacter();
 	
-	virtual void Tick(float DeltaTime) override;
-	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputMapping;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
 
 protected:
 	
 	virtual void BeginPlay() override;
+	
+	void Move(const FInputActionInstance& Instance);
+	void Look(const FInputActionInstance& Instance);
 
 private:
 	

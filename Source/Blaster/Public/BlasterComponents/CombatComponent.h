4d1d0cdd0e9df4e3e3b783f7e73ca4e368c549c8
Ansh Sharma <anshjangra15@gmail.com>
2026-06-26 18:16:37 +0530

    Replicating the HitTarget

--- Source/Blaster/Public/BlasterComponents/CombatComponent.h
+++ Source/Blaster/Public/BlasterComponents/CombatComponent.h
@@ -40,10 +40,10 @@ protected:
 	void FireButtonPressed(bool bPressed);
 	
 	UFUNCTION(Server, Reliable)
-	void ServerFire();
+	void ServerFire(const FVector_NetQuantize& TraceHitTarget);
 
 	UFUNCTION(NetMulticast, Reliable)
-	void MulticastFire();
+	void MulticastFire(const FVector_NetQuantize& TraceHitTarget);
 	
 	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
 	
@@ -65,7 +65,5 @@ private:
 	float AimWalkSpeed;
 	
 	bool bFireButtonPressed;
-	
-	FVector HitTarget;
 	
 };

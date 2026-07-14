ba8162cbfe288dc532b6226e67ec6c383c293fac
Ansh Sharma <anshjangra15@gmail.com>
2026-07-15 02:24:36 +0530

    Health Pickup

---
+++ Source/Blaster/Private/Pickups/HealthPickup.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Pickups/HealthPickup.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Character/BlasterCharacter.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+		
+	}
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{	
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+	Super::Destroyed();
+}

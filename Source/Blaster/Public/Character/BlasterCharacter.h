fb53dfb739f0a88e0d190d71cfa27ed0fed02816
Ansh Sharma <anshjangra15@gmail.com>
2026-08-01 12:14:02 +0530

    Maps added

--- Source/Blaster/Public/Character/BlasterCharacter.h
+++ Source/Blaster/Public/Character/BlasterCharacter.h
@@ -141,7 +141,7 @@ public:
 	FORCEINLINE UAnimMontage* GetReloadMontage() const { return ReloadMontage; }
 	FORCEINLINE UStaticMeshComponent* GetAttachedGrenade() const { return AttachedGrenade; }
 	FORCEINLINE ULagCompensationComponent* GetLagCompensation() const { return LagCompensation; }
-	FORCEINLINE bool IsHoldingTheFlag() const;
+	bool IsHoldingTheFlag() const;
 	bool IsLocallyReloading();
 	ETeam GetTeam();
 	void SetHoldingTheFlag(bool bHolding);
